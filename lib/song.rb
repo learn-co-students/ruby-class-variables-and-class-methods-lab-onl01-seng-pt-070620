e91fd2538e1406d609650b535ac9ee3536625ce4
Jill Short <jillian.b.short@gmail.com>
2020-07-24 23:12:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -28,20 +28,28 @@ class Song
   end
   
   def self.genre_count
-    @@genre_count = {}
+    genre_count = {}
       @@genres.each do |genre|
-        @@genre_count[genre] = 
-        
-        
-#         arr = [1,2,4,5,4,7] (1..6).group_by { |i| i%3 }
-
-# Hash[arr.group_by{|x|x}.map{|num,arr| [num, arr.size] }] 
-          
+        if genre_count[genre]
+          genre_count[genre]+=1 
+        else 
+          genre_count[genre]=1 
+        end
       end
+      genre_count
   end
   
+  
   def self.artist_count
-    @@artist_count = {}
+    artist_count = {}
+      @@artists.each do |artist|
+        if artist_count[artist]
+          artist_count[artist] += 1 
+        else
+          artist_count[artist] = 1 
+        end
+      end
+      artist_count
   end
   
 end
